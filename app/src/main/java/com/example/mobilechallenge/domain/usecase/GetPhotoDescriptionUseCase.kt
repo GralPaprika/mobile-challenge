810e117f364111bf485e84e77082f2faa46400ae
GralPaprika <dev@carlosrdgz.space>
2025-11-22 16:07:18 -0600

    feat: move PhotoDetailsRepository and its implementation for photo description retrieval

--- app/src/main/java/com/example/mobilechallenge/domain/usecase/GetPhotoDescriptionUseCase.kt
+++ app/src/main/java/com/example/mobilechallenge/domain/usecase/GetPhotoDescriptionUseCase.kt
@@ -2,7 +2,6 @@ package com.example.mobilechallenge.domain.usecase
 
 import com.example.mobilechallenge.domain.repository.PhotoDetailsRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
 
 interface GetPhotoDescriptionUseCase {
     operator fun invoke(): Flow<Result<String>>
